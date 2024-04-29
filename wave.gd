b5a7a121bad2f92b8babb4ae6d5b6fedc10c9cae
OliveADay <sharinggon23@gmail.com>
2024-04-29 12:22:52 -0700

    s

--- wave.gd
+++ wave.gd
@@ -1,6 +1,10 @@
 extends Sprite2D
+@export var speed = 10
+@export var maxPos = 300
 
+var prevPos = 1
 
+signal moveToFar()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +12,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	position.y -= 300*delta
+	if position.y <= maxPos and prevPos >= maxPos:
+		moveToFar.emit()
+		
+	prevPos = position.y
+		
+
+
+
+func _on_wave_6_move_to_far():
+	position.y = 295 # Replace with function body.
 	
+func activate_HeightPos():
+	maxPos = 201
