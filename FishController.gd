7c90f3f7fea3073f181effdb6d94e195bb2d220b
OliveADay <sharinggon23@gmail.com>
2024-05-01 06:53:07 -0700

    s

--- FishController.gd
+++ FishController.gd
@@ -7,11 +7,17 @@ var startMovingUp = false
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var xAxis = Input.get_axis("move_left", "move_right")
+	var yAxis = Input.get_axis("move_up", "move_down")
+	if xAxis:
+		velocity.x = xAxis * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if yAxis:
+		velocity.y = yAxis * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 	if startMovingUp:
 		if position.y > 200:
 			velocity.y = -100
