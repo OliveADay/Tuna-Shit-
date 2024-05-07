ef5a4ce612610556f2826d2b271217aba1c65be8
OliveADay <sharinggon23@gmail.com>
2024-05-07 06:41:54 -0700

    s

--- shit_bit.gd
+++ shit_bit.gd
@@ -1,11 +1,19 @@
 extends RigidBody2D
-
+var rand = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var rand = randi()%4
-		
-
+	rand = randi()%4
+	if rand == 0:
+		linear_velocity = Vector2(0, 100)
+		constant_torque = 90
+	if rand == 1:
+		linear_velocity = Vector2(0,-300)
+		constant_torque = 90
+	if rand ==2:
+		linear_velocity = Vector2(300,0)
+	if rand == 3:
+		linear_velocity = Vector2(-300,0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
