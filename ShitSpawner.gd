b42e296818b36969bbc9433511ba49efa6162bef
OliveADay <sharinggon23@gmail.com>
2024-05-09 20:55:46 -0700

    s

--- ShitSpawner.gd
+++ ShitSpawner.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var bScoreTxt = Label
 var time = 0.0
 var shits = [preload("res://shit.tscn"), preload("res://shit.tscn"), preload("res://shit.tscn"), preload("res://shit.tscn")]
 @export var interval = Vector2(2, 0.3)
@@ -8,6 +9,8 @@ var active = false
 var score = 0
 var scoreExel = 1
 var scoretext_late = ""
+var scoreFilePath = "user://score.save"
+var bScore = 0
 @export var scoreTxt = Label
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -36,8 +39,23 @@ func _process(delta):
 		else:
 			time += delta
 		scoretext_late = scoreTxt.text
+	LoadBScore()
+	bScoreTxt.text = "Best Score: " + str(bScore).pad_decimals(0)
+	if bScore < score:
+		SaveBScore()
 		#figure out how to reset everything else about the nodes so that they can be reintroduced
 
 
 func _on_label_key_pressed():
 	active = true # Replace with function body.
+	
+func SaveBScore():
+	var file = FileAccess.open(scoreFilePath, FileAccess.WRITE)
+	file.store_var(score)
+	
+func LoadBScore():
+	if FileAccess.file_exists(scoreFilePath):
+		var file = FileAccess.open(scoreFilePath, FileAccess.READ)
+		bScore = file.get_var(score)
+	else:
+		bScore = 0
